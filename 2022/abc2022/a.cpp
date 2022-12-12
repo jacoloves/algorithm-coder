0d054c04a22e2ffd9700497a1ccdd98fc8412d8f
jacoloves <5511068t@gmail.com>
2022-12-12 09:45:15 +0900

    20221212

---
+++ 2022/abc2022/a.cpp
@@ -0,0 +1,38 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <regex> // regex
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    if ((a == 5 && b == 5) || (b == 5 && c== 5) || (c == 5 && a == 5)) {
+        if (a == 7 || b == 7 || c == 7) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
---
+++ 2022/abc2022/b.cpp
@@ -0,0 +1,48 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <regex> // regex
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main() {
+    int n, l;
+    cin >> n >> l;
+
+    vector<string> v(n);
+
+    for (int i=0; i<n; ++i) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    string ans_str = "";
+
+    for (int i=0; i<n; ++i) {
+        ans_str += v[i];
+    }
+
+    cout << ans_str << endl;
+
+
+    return 0;
+}
