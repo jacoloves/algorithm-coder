845dcf2ed2343beaad1c791e22c739bffc1d16d7
jacoloves <5511068t@gmail.com>
2023-01-29 23:22:51 +0900

    20230129

---
+++ 2023/algorithm-book/chap2/2_2_4.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>  // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <bitset>     // bitset
+#include <cctype>     // isupper, islower, isdigit, toupper, tolower
+#include <cmath>
+#include <cstdint>        // int64_t, int*_t
+#include <cstdio>         // printf
+#include <deque>          // deque
+#include <iostream>       // cout, endl, cin
+#include <map>            // map
+#include <queue>          // queue, priority_queue
+#include <regex>          // regex
+#include <set>            // set
+#include <stack>          // stack
+#include <string>         // string, to_string, stoi
+#include <tuple>          // tuple, make_tuple
+#include <unordered_map>  // unordered_map
+#include <unordered_set>  // unordered_set
+#include <utility>        // pair, make_pair
+#include <vector>         // vector
+
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+using P = pair<int, int>;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        int a;
+        cin >> a;
+        sum += a;
+    }
+
+    cout << sum % 100 << endl;
+    return 0;
+}
